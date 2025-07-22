bfa3df2d87aad4aa4c8864606d563cb8f0cfcff1
Duru C. Michael <durumikaelc@gmail.com>
2025-07-22 14:24:15 +0100

    implement shadcn and auth setup with clerk

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		rules: {
+			"@eslint/no-unused-vars": [
+				"warn",
+				{
+					vars: "all",
+					args: "after-used",
+					ignoreRestSiblings: true,
+				},
+			],
+		},
+	},
 ];
 
 export default eslintConfig;
